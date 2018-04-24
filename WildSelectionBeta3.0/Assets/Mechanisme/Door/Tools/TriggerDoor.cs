b79e8753fddeb7950d25fe6e140656a96beaa8b1
Lfirst <lulu101m@yahoo.fr>
2018-04-24 15:37:08 +0200

    AjoutDesAssets
    En esperant que sa passe pas

---
+++ WildSelectionBeta3.0/Assets/Mechanisme/Door/Tools/TriggerDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour {
+
+	public GameObject TheDoor;
+	private float coorY;
+
+	void Start () {
+		coorY = TheDoor.transform.position.y;
+	}
+
+	private void OnTriggerStay(){
+		if (coorY + 3.5 > TheDoor.transform.position.y)
+			TheDoor.transform.position += TheDoor.transform.up * Time.deltaTime;
+	}
+
+	private void OnTriggerExit(){
+		while (coorY < TheDoor.transform.position.y) 
+			TheDoor.transform.position -= TheDoor.transform.up * Time.deltaTime;
+
+		}
+
+}
